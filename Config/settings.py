eef8af9c57545adddcd1dbac23618a8b5e1d3c2a
sh1mu7 <hasaanshimul@gmail.com>
2024-10-03 22:38:47 +0600

    domain name fix

--- Config/settings.py
+++ Config/settings.py
@@ -6,7 +6,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*','127.0.0.1','http://www.hasanmahmudul.xyz/']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
