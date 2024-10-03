cb1ee449845c899c9ca071330e2362bb78ced542
sh1mu7 <hasaanshimul@gmail.com>
2024-10-03 18:12:49 +0600

    .env and requirements.txt added

--- Config/settings/development.py
+++ Config/settings/development.py
@@ -6,7 +6,7 @@ ALLOWED_HOSTS = ['*', ]
 
 
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get(config("DATABASE_URL")))
+    "default": dj_database_url.parse(os.environ.get("postgresql://backend_port_user:1BVggA1JJLPovF3EkS0arr9ObV6SC1mA@dpg-crv7skg8fa8c73cstfdg-a.oregon-postgres.render.com/backend_port"))
 }
 
 STATIC_URL = '/static/'
